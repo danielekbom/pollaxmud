73dc56ce6c1b70775d204fa066a9100659951a3d
danielekbom <daniel5.2@hotmail.com>
2014-10-24 13:39:54 +0200

    börjat med worldimporter
    programmet hittar dock inte textfilen

--- src/pollaxmud/utilities/WorldImporter.java
+++ src/pollaxmud/utilities/WorldImporter.java
@@ -1,11 +1,36 @@
 package pollaxmud.utilities;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import pollaxmud.world.Room;
 import pollaxmud.world.World;
 
 public class WorldImporter {
 
 	public static World ImportWorld(){
 		World importedWorld = new World();
+		
+		try{
+			File file = new File("./world.txt");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+			String[] data;
+			Room roomToAdd;
+			while((line = bufferedReader.readLine()) != null){
+				data = line.split(",");
+				roomToAdd = new Room(data[0],Boolean.parseBoolean(data[5]));
+				importedWorld.addRoom(roomToAdd);
+			}
+			fileReader.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
 		return importedWorld;
 	}
 	
