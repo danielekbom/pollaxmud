cab8d5c63881416bc9a034292afa67297f683579
danielekbom <daniel5.2@hotmail.com>
2014-10-27 23:58:22 +0100

    Fixat så att man inte kan gå dit rum inte finns

--- src/pollaxmud/handlers/InputHandler.java
+++ src/pollaxmud/handlers/InputHandler.java
@@ -6,20 +6,35 @@ public class InputHandler {
 
 	public static void handleInput(String inputString, Player player){
 		if(inputString.equalsIgnoreCase("north")){
-			player.walkNorth();
-			player.getCurrentLocation().printEntranceText();
-		}
-		if(inputString.equalsIgnoreCase("east")){
-			player.walkEast();
-			player.getCurrentLocation().printEntranceText();
-		}
-		if(inputString.equalsIgnoreCase("south")){
-			player.walkSouth();
-			player.getCurrentLocation().printEntranceText();
-		}
-		if(inputString.equalsIgnoreCase("west")){
-			player.walkWest();
-			player.getCurrentLocation().printEntranceText();
+			if(player.getCurrentLocation().getRoomNorth() != null){
+				player.walkNorth();
+				player.getCurrentLocation().printEntranceText();
+			}else{
+				System.out.println("Can not walk to the north!");
+			}
+		}else if(inputString.equalsIgnoreCase("east")){
+			if(player.getCurrentLocation().getRoomEast() != null){
+				player.walkEast();
+				player.getCurrentLocation().printEntranceText();
+			}else{
+				System.out.println("Can not walk to the east!");
+			}
+		}else if(inputString.equalsIgnoreCase("south")){
+			if(player.getCurrentLocation().getRoomSouth() != null){
+				player.walkSouth();
+				player.getCurrentLocation().printEntranceText();
+			}else{
+				System.out.println("Can not walk to the south!");
+			}
+		}else if(inputString.equalsIgnoreCase("west")){
+			if(player.getCurrentLocation().getRoomWest() != null){
+				player.walkWest();
+				player.getCurrentLocation().printEntranceText();
+			}else{
+				System.out.println("Can not walk to the west!");
+			}
+		}else{
+			System.out.println("Unknown command!");
 		}
 	}
 	
