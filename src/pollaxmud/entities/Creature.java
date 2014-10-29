40561c12bcac31b6c22e13a65c3694755ba90168
Oscar Sandén <oscar.sanden.3819@student.uu.se>
2014-10-29 21:43:47 +0100

    Lite kommentarer och småfix om Teachers.

--- src/pollaxmud/entities/Creature.java
+++ src/pollaxmud/entities/Creature.java
@@ -1,13 +1,26 @@
 package pollaxmud.entities;
 
+/**
+ * The creature class. For all types of creatures.
+ * @author oscar
+ *
+ */
 public class Creature {
 	protected String Name;
 	protected CreatureType Type;
 	
+	/**
+	 * Get the name of a creature.
+	 * @return The name of the creature as a String
+	 */
 	public String getName() {
 		return Name;
 	}
 	
+	/**
+	 * Get the type of the creature (Teacher, sphinx, etc).
+	 * @return The type of the creature as a String.
+	 */
 	public CreatureType getType() {
 		return Type;
 	}
--- src/pollaxmud/entities/Teacher.java
+++ src/pollaxmud/entities/Teacher.java
@@ -3,9 +3,22 @@ package pollaxmud.entities;
 public class Teacher extends Creature {
 	private String Course;
 	
+	/**
+	 * Constructor for a Teacher.
+	 * @param name The name of the teacher
+	 * @param course The course the teacher is teaching.
+	 */
 	public Teacher(String name, String course) {
 		this.Name = name;
 		this.Course = course;
 		this.Type = CreatureType.TEACHER;
+	}
+	
+	/**
+	 * Get the course the teacher is teaching in.
+	 * @return The course name as a String
+	 */
+	public String getCourse() {
+		return Course;
 	}
 }
