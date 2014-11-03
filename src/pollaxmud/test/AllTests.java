00331f266fface66f73bb7594e6bb77ad4b68260
Oscar Sandén <oscar.sanden.3819@student.uu.se>
2014-11-03 21:20:16 +0100

    Teacher tests

--- src/pollaxmud/test/AllTests.java
+++ src/pollaxmud/test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ RoomTest.class , PlayerTest.class, BackpackTest.class })
+@SuiteClasses({ RoomTest.class , PlayerTest.class, BackpackTest.class, TeacherTest.class })
 public class AllTests {
 
 }
---
+++ src/pollaxmud/test/TeacherTest.java
@@ -0,0 +1,31 @@
+package pollaxmud.test;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.Test;
+
+import pollaxmud.entities.Book;
+import pollaxmud.entities.Course;
+import pollaxmud.entities.Question;
+import pollaxmud.entities.Teacher;
+
+public class TeacherTest {
+	private Book testItem1 = new Book("Test book 1", "Test author 1", "1999", 3);
+	private Course testCourse = new Course("TestCourse", testItem1, 10);
+	private Teacher testTeacher = new Teacher("TestTeacher", testCourse);
+	private Question testQuestion = new Question("What is my favourite color?", "Blue", "Red", "Green");
+	
+	@Test
+	public void testTeacherGetters() {
+		assertEquals(testTeacher.getCourse(), testCourse);
+		assertEquals(testTeacher.getCourseName(), "TestCourse");
+	}
+
+	@Test
+	public void testTeacherAskQuestion() {
+		// TODO: This shit...
+		//fail("Not implemented teacher askQuestion test.");
+	}
+}
