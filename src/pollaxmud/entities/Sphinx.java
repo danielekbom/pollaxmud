9bdedd5a8f96110f2e050f0ffe83a23e19b8155a
Oscar Sandén <oscar.sanden.3819@student.uu.se>
2014-11-04 09:24:27 +0100

    Skapat Sphinx.java

---
+++ src/pollaxmud/entities/Sphinx.java
@@ -0,0 +1,11 @@
+package pollaxmud.entities;
+
+import pollaxmud.enums.CreatureType;
+
+public class Sphinx extends Creature{
+
+	public Sphinx () {
+		this.Name = "Sphinx of Pollax";
+		this.Type = CreatureType.SPHINX;
+	}
+}
