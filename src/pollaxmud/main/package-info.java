00822391dab3cfb1b05ab5bb10f32549e403fc0d
danielekbom <daniel5.2@hotmail.com>
2014-11-05 17:11:25 +0100

    Dokumentation för packages

---
+++ src/pollaxmud/main/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * The main package which contains the Pollaxmud class which is the starting point of the game.
+ * @author Daniel and Oscar
+ *
+ */
+package pollaxmud.main;
---
+++ src/pollaxmud/test/WorldTest.java
@@ -0,0 +1,14 @@
+package pollaxmud.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WorldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
